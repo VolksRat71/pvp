b365e4a0a7bcd70d2658041d47bb0a01efc1d147
Nate Ryan <nathanieljryan1994@gmail.com>
2023-09-25 13:18:40 -0500

    fetch and refetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Container, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import { Button, CircularProgress, Container, Table, TableBody, TableCell, TableHead, TableRow, Typography, Box } from '@mui/material';
 
 function App() {
   const [allDevices, setAllDevices] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchAllDevices = () => {
+    setIsLoading(true);
     fetch('http://127.0.0.1:5000/discover_all')
       .then(response => response.json())
       .then(data => {
         setAllDevices(data.devices);
+        setIsLoading(false);
       })
       .catch(error => {
         console.error('Error fetching all devices:', error);
+        setIsLoading(false);
       });
   };
 
   const connectToDevice = (address) => {
-    // Logic to connect to the device using its address.
-    // This can be updated based on how you plan to implement the connection logic.
     console.log("Connecting to device:", address);
   };
 
-  useEffect(fetchAllDevices, []);
+  useEffect(() => {
+    // Initially load the devices when the component mounts
+    fetchAllDevices();
+  }, []);
 
   return (
     <Container>
@@ -29,28 +34,40 @@ function App() {
         All Devices
       </Typography>
 
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Address</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {allDevices.map(device => (
-            <TableRow key={device.Address}>
-              <TableCell>{device.Address}</TableCell>
-              <TableCell>{device.Name}</TableCell>
-              <TableCell align="right">
-                <Button variant="contained" color="primary" onClick={() => connectToDevice(device.Address)}>
-                  Connect to this Device
-                </Button>
-              </TableCell>
+      <Box display="flex" justifyContent="center" marginBottom="20px">
+        <Button variant="contained" color="primary" onClick={fetchAllDevices}>
+          Discover All Devices
+        </Button>
+      </Box>
+
+      {isLoading ? (
+        <Box display="flex" justifyContent="center">
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Address</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Action</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {allDevices.map(device => (
+              <TableRow key={device.Address}>
+                <TableCell>{device.Address}</TableCell>
+                <TableCell>{device.Name}</TableCell>
+                <TableCell align="right">
+                  <Button variant="contained" color="primary" onClick={() => connectToDevice(device.Address)}>
+                    Connect to this Device
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </Container>
   );
 }
