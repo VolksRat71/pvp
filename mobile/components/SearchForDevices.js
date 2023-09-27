af5892d92905838b8d8ed0648c3dde9a94801955
Nate Ryan <nathanieljryan1994@gmail.com>
2023-09-27 10:01:01 -0500

    animate

--- mobile/components/SearchForDevices.js
+++ mobile/components/SearchForDevices.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { Button } from 'react-native-paper';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Button, ActivityIndicator } from 'react-native-paper';
 import { searchDevices } from '../utils/bleManager';
 
 const SearchForDevices = ({ onDevicesFound }) => {
+    const [loading, setLoading] = useState(false);
+
     const handleSearch = async () => {
+        setLoading(true);
         const devices = await searchDevices();
         onDevicesFound(devices);
+        setLoading(false);
     };
 
     return (
-        <Button mode="contained" onPress={handleSearch}>
-            Search For Devices
-        </Button>
+        <View>
+            <Button mode="contained" onPress={handleSearch} disabled={loading}>
+                Search For Devices
+            </Button>
+            {loading && <ActivityIndicator style={{marginTop: 30}} animating={true} color="#186374" />}
+        </View>
     );
 }
 
--- mobile/views/DeviceConnection.js
+++ mobile/views/DeviceConnection.js
@@ -1,32 +1,68 @@
 // ./views/DeviceConnection.js
 
-import React, { useState } from 'react';
-import { ScrollView } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { ScrollView, Animated } from 'react-native';
 import { DataTable } from 'react-native-paper';
 import SearchForDevices from '../components/SearchForDevices';
+import { searchDevices } from '../utils/bleManager';
 
 const DeviceConnection = () => {
     const [devices, setDevices] = useState([]);
+    const slideAnim = useRef(new Animated.Value(0)).current;  // Initial value for translateY: 0
 
-    const handleDevicesFound = (foundDevices) => {
-        setDevices(foundDevices);
+    const slideAway = () => {
+        // Slide out animation
+        Animated.timing(
+            slideAnim,
+            {
+                toValue: -500,
+                duration: 500,
+                useNativeDriver: true
+            }
+        ).start();
+    };
+
+    const slideIn = () => {
+        // Slide in animation
+        Animated.timing(
+            slideAnim,
+            {
+                toValue: 0,
+                duration: 1000,
+                useNativeDriver: true
+            }
+        ).start();
+    };
+
+    const handleSearch = async () => {
+        slideAway();
+        setDevices([]);  // Clear the devices
+
+        // Simulating a delay for the search (can be removed if the search is naturally slow)
+        setTimeout(async () => {
+            const foundDevices = await searchDevices();
+            setDevices(foundDevices);
+            slideIn();
+        }, 1000);
     };
 
     return (
         <ScrollView style={{ padding: 20 }}>
-            <SearchForDevices onDevicesFound={handleDevicesFound} />
-            <DataTable>
-                <DataTable.Header>
-                    <DataTable.Title>Address</DataTable.Title>
-                    <DataTable.Title>Name</DataTable.Title>
-                </DataTable.Header>
-                {devices.map((device, index) => (
-                    <DataTable.Row key={index}>
-                        <DataTable.Cell>{device.address}</DataTable.Cell>
-                        <DataTable.Cell>{device.name}</DataTable.Cell>
-                    </DataTable.Row>
-                ))}
-            </DataTable>
+            <SearchForDevices onDevicesFound={handleSearch} />
+            <Animated.View style={{ transform: [{ translateY: slideAnim }] }}>
+                <DataTable>
+                    <DataTable.Header>
+                        <DataTable.Title>Address</DataTable.Title>
+                        <DataTable.Title>Name</DataTable.Title>
+                    </DataTable.Header>
+                    {devices.map((device, index) => (
+                        <DataTable.Row key={index}>
+                            <DataTable.Cell>{device.address}</DataTable.Cell>
+                            <DataTable.Cell>{device.name}</DataTable.Cell>
+                        </DataTable.Row>
+                    ))}
+                </DataTable>
+            </Animated.View>
         </ScrollView>
     );
 }
