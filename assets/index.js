b5c88ce3d8b1c28ad0853ece8fb1106c22bcc728
Nate Ryan <nathanieljryan1994@gmail.com>
2023-09-29 17:18:02 -0500

    pushing then cleaning

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 // Constants and Global Variables
 let ageInputChanged = false;
+let rangeValidationResult = false;
 const entries = JSON.parse(localStorage.getItem('entries') || '[]');
 let currentSettings = {
     easy: { lowerBound: 0.5, upperBound: 1.35, maxScore: 90 },
@@ -67,7 +68,7 @@ function updateRangeHint() {
 function validateGoalHeartRateStart() {
     const age = parseFloat(document.getElementById('age').value) || 25;
     const difficulty = document.getElementById('difficulty').value;
-    let goalHeartRateStart = parseFloat(document.getElementById('goalHeartRateStart').value) || 0;
+    let goalHeartRateStart = parseFloat(document.getElementById('goalHeartRateStart').value);
 
     const scorer = new HeartRateScorer(age, goalHeartRateStart, difficulty);
 
@@ -79,22 +80,40 @@ function validateGoalHeartRateStart() {
     maxHint.classList.remove('text-danger');
     minHint.classList.add('text-muted');
     maxHint.classList.add('text-muted');
+    rangeValidationResult = true;
 
     // Set hints to default values
     minHint.textContent = `Minimum safe heart rate: ${scorer.minimumSafeHeartRate().toFixed(0)}`;
     maxHint.textContent = `Maximum heart rate: ${scorer.MHR.toFixed(0)}`;
 
-    if (goalHeartRateStart + scorer.getDifficultyRangeWidth() > scorer.MHR) {
-        maxHint.textContent = `Your input is too high! Maximum heart rate: ${scorer.MHR.toFixed(0)}`;
-        maxHint.classList.remove('text-muted');
-        maxHint.classList.add('text-danger');
-    } else if (goalHeartRateStart < scorer.minimumSafeHeartRate()) {
-        minHint.textContent = `Your input is too low! Minimum safe heart rate: ${scorer.minimumSafeHeartRate().toFixed(0)}`;
-        minHint.classList.remove('text-muted');
-        minHint.classList.add('text-danger');
+    if(goalHeartRateStart !== null || goalHeartRateStart !== undefined) {
+        if (goalHeartRateStart + scorer.getDifficultyRangeWidth() > scorer.MHR) {
+            rangeValidationResult = false;
+            maxHint.textContent = `Your input is too high! Maximum heart rate: ${scorer.MHR.toFixed(0)}`;
+            maxHint.classList.remove('text-muted');
+            maxHint.classList.add('text-danger');
+        } else if (goalHeartRateStart < scorer.minimumSafeHeartRate()) {
+            rangeValidationResult = false;
+            minHint.textContent = `Your input is too low! Minimum safe heart rate: ${scorer.minimumSafeHeartRate().toFixed(0)}`;
+            minHint.classList.remove('text-muted');
+            minHint.classList.add('text-danger');
+        }
     }
 
     document.getElementById('goalHeartRateEnd').value = (goalHeartRateStart + scorer.getDifficultyRangeWidth()).toFixed(0);
+}
+
+function initRangeHint() {
+    const age = parseFloat(document.getElementById('age').value) || 25;
+    const difficulty = document.getElementById('difficulty').value;
+    let goalHeartRateStart = parseFloat(document.getElementById('goalHeartRateStart').value) || 0;
+
+    const scorer = new HeartRateScorer(age, goalHeartRateStart, difficulty);
+    const minHint = document.getElementById('minHeartRateHint');
+    const maxHint = document.getElementById('maxHeartRateHint');
+
+    minHint.textContent = `Minimum safe heart rate: ${scorer.minimumSafeHeartRate().toFixed(0)}`;
+    maxHint.textContent = `Maximum heart rate: ${scorer.MHR.toFixed(0)}`;
 }
 
 function debounce(func, wait) {
@@ -106,20 +125,19 @@ function debounce(func, wait) {
 }
 
 
-const debouncedValidation = debounce(validateGoalHeartRateStart, 1000);
+const debouncedValidation = debounce(validateGoalHeartRateStart, 1500);
 
 function checkFormCompletion() {
     const age = parseFloat(document.getElementById('age').value);
     const goalHeartRateStart = parseFloat(document.getElementById('goalHeartRateStart').value);
     const difficulty = document.getElementById('difficulty').value;
-
     const calculateButton = document.getElementById('calculateButton');
 
     // Check if all form fields are filled out
     if (!isNaN(age) && !isNaN(goalHeartRateStart) && difficulty) {
-        calculateButton.disabled = false;
+        calculateButton.disabled = false && !rangeValidationResult;
     } else {
-        calculateButton.disabled = true;
+        calculateButton.disabled = true && !rangeValidationResult;
     }
 }
 
@@ -186,3 +204,4 @@ function deleteEntry(index) {
 updateRangeHint();
 displayEntries();
 checkFormCompletion()
+initRangeHint()
